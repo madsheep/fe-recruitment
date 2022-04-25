de30c7e8f5760eac7184c24d58cae13267d426f4
Sebastian <s.superczynski@gmail.com>
2022-04-25 11:37:03 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'sb-fe';
+export class AppComponent implements OnInit{
+  threads: any
+  selected: any
+  constructor(private readonly http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.http.get("https://recruapi.herokuapp.com/threads").subscribe((response) => {
+      console.log('response', response)
+      this.threads = response
+    })
+  }
+
+  selectThread(thread: any) {
+    this.selected = thread
+  }
 }
